07fca3612c1503e353a8455e2808b22c5521b925
chefeleUNH <chefele@newhaven.edu>
2020-02-26 07:02:07 -0500

    added tests for create, read, and delete

--- PizzaHub/ContentView.swift
+++ PizzaHub/ContentView.swift
@@ -9,23 +9,92 @@
 import SwiftUI
 import FirebaseFirestore
 
+let db = Firestore.firestore()
+
 struct ContentView: View {
     var body: some View {
-        Text("Firebase Console Test")
-        .onAppear() {
-            let db = Firestore.firestore()
-            db.collection("pizzerias").getDocuments() { (querySnapshot, err) in
-                if let err = err {
-                    print("Error getting documents: \(err)")
-                } else {
-                    for document in querySnapshot!.documents {
-                        print("\(document.documentID) => \(document.data())")
-                    }
-                }
+        VStack {
+            Button(action: {
+                createPizzerias()
+            }) {
+                Text("Create Pizzerias")
+                    .font(.largeTitle)
+            }
+            .padding(10.0)
+            
+            Button(action: {
+                getCollection(collection: "pizzerias")
+            }) {
+                Text("Get All Pizzerias")
+                    .font(.largeTitle)
+            }
+            .padding(10.0)
+            
+            Button(action: {
+                deleteCollection(collection: "pizzerias")
+            }) {
+                Text("Delete All Pizzerias")
+                    .font(.largeTitle)
+            }
+            .padding(10.0)
+        }
+    }
+}
+
+private func createPizzerias() {
+    let pizzeriasRef = db.collection("pizzerias")
+    
+    pizzeriasRef.document().setData([
+        "name": "Vetrano's",
+        "city": "Westerly",
+        "state": "RI"
+    ])
+    
+    pizzeriasRef.document().setData([
+        "name": "Vittoria's",
+        "city": "Westerly",
+        "state": "RI"
+    ])
+
+    pizzeriasRef.document().setData([
+           "name": "Pizza Today",
+           "city": "Groton",
+           "state": "CT"
+    ])
+
+    pizzeriasRef.document().setData([
+        "name": "Midway Pizza",
+        "city": "Groton",
+        "state": "CT"
+    ])
+}
+
+private func getCollection(collection: String) {
+    db.collection(collection).getDocuments() { (querySnapshot, err) in
+        if let err = err {
+            print("Error getting documents: \(err)")
+        } else {
+            for document in querySnapshot!.documents {
+                print("\(document.documentID) => \(document.data())")
             }
         }
     }
 }
+
+private func deleteCollection(collection: String) {
+    db.collection(collection).getDocuments() { (querySnapshot, err) in
+        if let err = err {
+            print("Error getting documents: \(err)")
+            return
+        }
+
+        for document in querySnapshot!.documents {
+            print("Deleting \(document.documentID) => \(document.data())")
+            document.reference.delete()
+        }
+    }
+}
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
