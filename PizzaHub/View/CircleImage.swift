1f62c6ebc7a82b3510388de4eb64abe4d3117721
chefeleUNH <chefele@newhaven.edu>
2020-04-07 22:56:50 -0400

    swapped photos out for square photos for better scaling and to prep for pizzeria row

--- PizzaHub/View/CircleImage.swift
+++ PizzaHub/View/CircleImage.swift
@@ -13,7 +13,6 @@ struct CircleImage: View {
     
     var body: some View {
         image
-            .frame(width: imgSize, height: imgSize)
             .clipShape(Circle())
             .overlay(Circle().stroke(Color.white, lineWidth: 4))
             .shadow(radius: 10)
