982c1483d08cb7364a5de881341c6fa2d13a53f9
chefeleUNH <chefele@newhaven.edu>
2021-01-12 22:26:41 -0500

    converted over to new 'App' protocol introduced in iOS 14

---
+++ PizzaHub/PizzaHubApp.swift
@@ -0,0 +1,20 @@
+//
+//  PizzaHubApp.swift
+//  PizzaHub
+//
+//  Created by Charles Hefele on 1/12/21.
+//  Copyright © 2021 Charles Hefele. All rights reserved.
+//
+
+import SwiftUI
+
+@main
+struct PizzaHubApp: App {
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
+
+    var body: some Scene {
+        WindowGroup {
+            AppTabView().environmentObject(ShoppingCart())
+        }
+    }
+}
