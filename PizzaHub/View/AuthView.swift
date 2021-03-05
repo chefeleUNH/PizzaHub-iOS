405dbdc0db5469d19bbc4a3837f02f15f1738b46
chefeleUNH <chefele@newhaven.edu>
2021-03-04 22:37:49 -0500

    updates to the AuthView screens

--- PizzaHub/View/AuthView.swift
+++ PizzaHub/View/AuthView.swift
@@ -21,6 +21,66 @@ struct SignInView: View {
     @State var error: String = ""
     @EnvironmentObject var session: FirebaseSession
     
+    var body: some View {
+        VStack {
+            LinearGradient(gradient: Gradient(colors: [.red, .orange]),
+                           startPoint: .leading,
+                           endPoint: .trailing)
+                .mask(Text("PizzaHub")
+                        .font(.system(size: 60, weight: .heavy)))
+            
+            Image("Pizza")
+                .resizable()
+                .scaledToFit()
+                .frame(width: 150, height: 150)
+            
+            VStack(spacing: 18) {
+                TextField("Email address", text: $email)
+                    .font(.system(size: 20))
+                    .padding(12)
+                    .background(RoundedRectangle(cornerRadius: 5).strokeBorder(Color.primary, lineWidth: 2))
+                
+                SecureField("Password", text: $password)
+                    .font(.system(size: 20))
+                    .padding(12)
+                    .background(RoundedRectangle(cornerRadius: 5).strokeBorder(Color.primary, lineWidth: 2))
+            }
+            .padding(.vertical, 16)
+            
+            Button(action: signIn) {
+                Text("Sign in")
+                    .frame(minWidth: 0, maxWidth: .infinity)
+                    .frame(height: 50)
+                    .foregroundColor(.black)
+                    .font(.system(size: 24, weight: .bold))
+                    .background(LinearGradient(gradient: Gradient(colors: [Color.red, Color.orange]), startPoint:  .leading, endPoint: .trailing))
+                    .cornerRadius(5)
+            }
+            
+            if (error != "") {
+                Text(error)
+                    .font(.system(size: 20, weight: .semibold))
+                    .foregroundColor(.red)
+                    .padding()
+            }
+            
+            NavigationLink(destination: SignUpView()) {
+                HStack {
+                    Text("I'm a new user.")
+                        .font(.system(size: 18, weight: .light))
+                        .foregroundColor(.primary)
+                    
+                    Text("Create an account")
+                        .font(.system(size: 18, weight: .semibold))
+                        .foregroundColor(.secondary)
+                }
+            }
+            
+            Spacer()
+        }
+        .padding(.horizontal, 32)
+    }
+    
     func signIn() {
         session.signIn(email: email, password: password) { (result, error) in
             if let error = error {
@@ -31,62 +91,6 @@ struct SignInView: View {
             }
         }
     }
-    
-    var body: some View {
-        VStack {
-            Text("Welcome Back!")
-                .font(.system(size: 32, weight: .heavy))
-            
-            Text("Sign in to continue")
-                .font(.system(size: 18, weight: .medium))
-                .foregroundColor(.gray)
-            
-            VStack(spacing: 18) {
-                TextField("Email address", text: $email)
-                    .font(.system(size: 14))
-                    .padding(12)
-                    .background(RoundedRectangle(cornerRadius: 5).strokeBorder(Color.primary, lineWidth: 1))
-                
-                SecureField("Password", text: $password)
-                    .font(.system(size: 14))
-                    .padding(12)
-                    .background(RoundedRectangle(cornerRadius: 5).strokeBorder(Color.primary, lineWidth: 1))
-            }
-            .padding(.vertical, 64)
-            
-            Button(action: signIn) {
-                Text("Sign in")
-                    .frame(minWidth: 0, maxWidth: .infinity)
-                    .frame(height: 50)
-                    .foregroundColor(.white)
-                    .font(.system(size: 14, weight: .bold))
-                    .background(LinearGradient(gradient: Gradient(colors: [Color.primary, Color.secondary]), startPoint:  .leading, endPoint: .trailing))
-                    .cornerRadius(5)
-            }
-            
-            if (error != "") {
-                Text(error)
-                    .font(.system(size: 14, weight: .semibold))
-                    .foregroundColor(.red)
-                    .padding()
-            }
-            
-            Spacer()
-            
-            NavigationLink(destination: SignUpView()) {
-                HStack {
-                    Text("I'm a new user.")
-                        .font(.system(size: 14, weight: .light))
-                        .foregroundColor(.primary)
-                    
-                    Text("Create an account")
-                        .font(.system(size: 14, weight: .semibold))
-                        .foregroundColor(.secondary)
-                }
-            }
-        }
-        .padding(.horizontal, 32)
-    }
 }
 
 struct SignUpView: View {
@@ -94,6 +98,53 @@ struct SignUpView: View {
     @State var password: String = ""
     @State var error: String = ""
     @EnvironmentObject var session: FirebaseSession
+    
+    var body: some View {
+        VStack {
+            LinearGradient(gradient: Gradient(colors: [.red, .orange]),
+                           startPoint: .leading,
+                           endPoint: .trailing)
+                .mask(Text("Create Account")
+                        .font(.system(size: 40, weight: .heavy)))
+            
+            Text("Sign up to get started")
+                .font(.system(size: 24, weight: .medium))
+                .foregroundColor(.primary)
+            
+            VStack(spacing: 18) {
+                TextField("Email address", text: $email)
+                    .font(.system(size: 20))
+                    .padding(12)
+                    .background(RoundedRectangle(cornerRadius: 5).strokeBorder(Color.primary, lineWidth: 2))
+                
+                SecureField("Password", text: $password)
+                    .font(.system(size: 20))
+                    .padding(12)
+                    .background(RoundedRectangle(cornerRadius: 5).strokeBorder(Color.primary, lineWidth: 2))
+            }
+            .padding(.vertical, 16)
+            
+            Button(action: signUp) {
+                Text("Create Account")
+                    .frame(minWidth: /*@START_MENU_TOKEN@*/0/*@END_MENU_TOKEN@*/, maxWidth: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/)
+                    .frame(height: 50)
+                    .foregroundColor(.black)
+                    .font(.system(size: 24, weight: .bold))
+                    .background(LinearGradient(gradient: Gradient(colors: [Color.red, Color.orange]), startPoint: .leading, endPoint: .trailing))
+                    .cornerRadius(5)
+            }
+            
+            if (error != "") {
+                Text(error)
+                    .font(.system(size: 20, weight: .semibold))
+                    .foregroundColor(.red)
+                    .padding()
+            }
+            
+            Spacer()
+            
+        }.padding(.horizontal, 32)
+    }
     
     func signUp() {
         session.signUp(email: email, password: password) { (result, error) in
@@ -104,50 +155,6 @@ struct SignUpView: View {
                 self.password = ""
             }
         }
-    }
-    
-    var body: some View {
-        VStack {
-            Text("Create Account")
-                .font(.system(size: 32, weight: .heavy))
-            
-            Text("Sign up to get started")
-                .font(.system(size: 18, weight: .medium))
-                .foregroundColor(.gray)
-            
-            VStack(spacing: 18) {
-                TextField("Email address", text: $email)
-                    .font(.system(size: 14))
-                    .padding(12)
-                    .background(RoundedRectangle(cornerRadius: 5).strokeBorder(Color.primary, lineWidth: 1))
-                
-                SecureField("Password", text: $password)
-                    .font(.system(size: 14))
-                    .padding(12)
-                .background(RoundedRectangle(cornerRadius: 5).strokeBorder(Color.primary, lineWidth: 1))
-            }.padding(.vertical, 64)
-            
-            Button(action: signUp) {
-                Text("Create Account")
-                    .frame(minWidth: /*@START_MENU_TOKEN@*/0/*@END_MENU_TOKEN@*/, maxWidth: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/)
-                    .frame(height: 50)
-                    .foregroundColor(.white)
-                    .font(.system(size: 14, weight: .bold))
-                    .background(LinearGradient(gradient: Gradient(colors: [Color.primary, Color.secondary]),
-                                               startPoint: .leading, endPoint: .trailing))
-                    .cornerRadius(5)
-            }
-            
-            if (error != "") {
-                Text(error)
-                    .font(.system(size: 14, weight: .semibold))
-                    .foregroundColor(.red)
-                    .padding()
-            }
-            
-            Spacer()
-            
-        }.padding(.horizontal, 32)
     }
 }
 
